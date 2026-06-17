3463bf4d8d2ed58d9f8d42a16c865ee149a49acc
Minh3132004 <vubinhminh04@gmail.com>
2026-06-17 17:19:02 +0700

    feat: add auth-service component

---
+++ auth-service/src/main/java/com/bookstore/auth/AuthServiceApplication.java
@@ -0,0 +1,15 @@
+package com.bookstore.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableAsync
+public class AuthServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+}
